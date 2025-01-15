e176e9ef2a176c33c15cccb714357358384fbcf0
sudo-louis <luisito.da.123@gmail.com>
2025-01-15 12:25:53 -0600

    Rutas protegidas por Middleware Sanctum

--- routes/api.php
+++ routes/api.php
@@ -22,33 +22,47 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Login y Register
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function() {
+    // Cerrar sesión
     Route::get('logout', [AuthController::class, 'logout']);
+
+    // Productos
+    Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
+    Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
+    Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
+
+    // Empleados
+    Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados.store');
+    Route::put('/empleados/{id}', [EmpleadoController::class, 'update'])->name('empleados.update');
+    Route::delete('/empleados/{id}', [EmpleadoController::class, 'destroy'])->name('empleados.destroy');
+
+    // Proveedores
+    Route::post('/proveedores', [ProveedorController::class, 'store'])->name('proveedores.store');
+    Route::put('/proveedores/{id}', [ProveedorController::class, 'update'])->name('proveedores.update');
+    Route::delete('/proveedores/{id}', [ProveedorController::class, 'destroy'])->name('proveedores.destroy');
+
+    // Clientes
+    Route::post('/clientes', [EmpleadoController::class, 'store'])->name('clientes.store');
+    Route::put('/clientes/{id}', [EmpleadoController::class, 'update'])->name('clientes.update');
+    Route::delete('/clientes/{id}', [EmpleadoController::class, 'destroy'])->name('clientes.destroy');
 });
 
+// Listar o seleccionar por ID Productos
 Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
 Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
-Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
-Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
-Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
 
+// Listar o seleccionar por ID Empleados
 Route::get('/empleados', [EmpleadoController::class, 'index'])->name('empleados.index');
 Route::get('/empleados/{id}', [EmpleadoController::class, 'show'])->name('empleados.show');
-Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados.store');
-Route::put('/empleados/{id}', [EmpleadoController::class, 'update'])->name('empleados.update');
-Route::delete('/empleados/{id}', [EmpleadoController::class, 'destroy'])->name('empleados.destroy');
 
+// Listar o seleccionar por ID Proveedores
 Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores.index');
 Route::get('/proveedores/{id}', [ProveedorController::class, 'show'])->name('proveedores.show');
-Route::post('/proveedores', [ProveedorController::class, 'store'])->name('proveedores.store');
-Route::put('/proveedores/{id}', [ProveedorController::class, 'update'])->name('proveedores.update');
-Route::delete('/proveedores/{id}', [ProveedorController::class, 'destroy'])->name('proveedores.destroy');
 
+// Listar o seleccionar por ID Clientes
 Route::get('/clientes', [EmpleadoController::class, 'index'])->name('clientes.index');
 Route::get('/clientes/{id}', [EmpleadoController::class, 'show'])->name('clientes.show');
-Route::post('/clientes', [EmpleadoController::class, 'store'])->name('clientes.store');
-Route::put('/clientes/{id}', [EmpleadoController::class, 'update'])->name('clientes.update');
-Route::delete('/clientes/{id}', [EmpleadoController::class, 'destroy'])->name('clientes.destroy');
