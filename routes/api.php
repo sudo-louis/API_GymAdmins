92ccdefb573e51f182b01a3a148ac69b1af6f69e
sudo-louis <luisito.da.123@gmail.com>
2025-01-15 12:36:28 -0600

    Update

--- routes/api.php
+++ routes/api.php
@@ -31,38 +31,30 @@ Route::middleware(['auth:sanctum'])->group(function() {
     Route::get('logout', [AuthController::class, 'logout']);
 
     // Productos
+    Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+    Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
     Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
     Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
     Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
 
     // Empleados
+    Route::get('/empleados', [EmpleadoController::class, 'index'])->name('empleados.index');
+    Route::get('/empleados/{id}', [EmpleadoController::class, 'show'])->name('empleados.show');
     Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados.store');
     Route::put('/empleados/{id}', [EmpleadoController::class, 'update'])->name('empleados.update');
     Route::delete('/empleados/{id}', [EmpleadoController::class, 'destroy'])->name('empleados.destroy');
 
     // Proveedores
+    Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores.index');
+    Route::get('/proveedores/{id}', [ProveedorController::class, 'show'])->name('proveedores.show');
     Route::post('/proveedores', [ProveedorController::class, 'store'])->name('proveedores.store');
     Route::put('/proveedores/{id}', [ProveedorController::class, 'update'])->name('proveedores.update');
     Route::delete('/proveedores/{id}', [ProveedorController::class, 'destroy'])->name('proveedores.destroy');
 
     // Clientes
+    Route::get('/clientes', [EmpleadoController::class, 'index'])->name('clientes.index');
+    Route::get('/clientes/{id}', [EmpleadoController::class, 'show'])->name('clientes.show');
     Route::post('/clientes', [EmpleadoController::class, 'store'])->name('clientes.store');
     Route::put('/clientes/{id}', [EmpleadoController::class, 'update'])->name('clientes.update');
     Route::delete('/clientes/{id}', [EmpleadoController::class, 'destroy'])->name('clientes.destroy');
 });
-
-// Listar o seleccionar por ID Productos
-Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
-Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
-
-// Listar o seleccionar por ID Empleados
-Route::get('/empleados', [EmpleadoController::class, 'index'])->name('empleados.index');
-Route::get('/empleados/{id}', [EmpleadoController::class, 'show'])->name('empleados.show');
-
-// Listar o seleccionar por ID Proveedores
-Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores.index');
-Route::get('/proveedores/{id}', [ProveedorController::class, 'show'])->name('proveedores.show');
-
-// Listar o seleccionar por ID Clientes
-Route::get('/clientes', [EmpleadoController::class, 'index'])->name('clientes.index');
-Route::get('/clientes/{id}', [EmpleadoController::class, 'show'])->name('clientes.show');
